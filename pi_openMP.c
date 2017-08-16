1a1df733428de20d9ff955b24b7a6fa1eaaa2f66
weili10 <lwei1993@gmail.com>
2017-08-16 12:03:24 +1000

    Update pi_openMP.c
    use reduction parameter to accumulate sum

--- pi_openMP.c
+++ pi_openMP.c
@@ -9,29 +9,22 @@ int main()
 {
     int i, t;
     double step, st, et, x, pi = 0.0;
-    double sum[MAX_THREAD] = {0.0};
+    double sum = 0.0;
 
     omp_set_num_threads(MAX_THREAD);
 
     step = 1.0/(double) NUM_STEPS;
     st = omp_get_wtime();
 
-    #pragma omp parallel for
+    #pragma omp parallel for reduction(+:sum)
     for (int i = 0; i < NUM_STEPS; ++i)
     {
-        t = omp_get_thread_num();
         x = (i+0.5) * step;
-        sum[t] = sum[t] + 4.0/(1.0 + x*x);
+        sum = sum + 4.0/(1.0 + x*x);
     }
 
     et = omp_get_wtime();
-
-    for (int i = 0; i < MAX_THREAD; ++i)
-    {
-        pi = pi + step * sum[i];
-        printf("sum[%d] = %lf\n", i, sum[i]);
-    }
-
+    pi = step * sum;
     printf("pi = %lf\n", pi);
     printf("run time = %lf s\n", et-st);
 
