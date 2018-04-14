f1de4ff480c870c4349ec821f36d8d12cec09ad0
weili10 <lwei1993@gmail.com>
2018-04-14 23:55:04 +1000

    Delete creatGraph.py

--- creatGraph.py
+++
@@ -1,9 +0,0 @@
-import random
-nodeNum = 100
-edge = 0
-print nodeNum
-for i in range(1,nodeNum+1):
-    for j in range(1,nodeNum+1):
-        if i != j:
-            edge = random.randint(1,20)
-            print i,j,edge
